dda0cb95901239777ed9db76bce7ac862b3170ea
TiGRoNdev <tigron.dev@gmail.com>
2018-01-29 17:35:32 +0000

    Added server-app, added configs for supervisor

---
+++ back/back/aio-server/main.py
@@ -0,0 +1,15 @@
+import argparse
+from aiohttp import web
+
+parser = argparse.ArgumentParser(description="aiohttp server example")
+parser.add_argument('--path')
+parser.add_argument('--port')
+
+
+if __name__ == '__main__':
+    app = web.Application()
+    # configure app
+    app.router.add_get('/', index)
+
+    args = parser.parse_args()
+    web.run_app(app, path=args.path, port=args.port)
