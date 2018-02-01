54da55d5cc7cf007376f68787862213d033a2641
TiGRoNdev <tigron.dev@gmail.com>
2018-02-01 18:18:41 +0000

    Small bug fixes

--- db1/shard2-slave.lua
+++ db1/shard2-slave.lua
@@ -12,7 +12,7 @@ box.cfg{
 	memtx_dir	=	'snaps',
 	vinyl_dir	=	'cold-data',
 	username	=	'student26',
-	memtx_memory	=	147483648,
+	memtx_memory	=	2147483648,
 	checkpoint_interval	=	1800
 }
 
@@ -21,10 +21,10 @@ box.schema.user.create('student26', {password = 'fobloi56', if_not_exists = true
 
 local cfg = {
 	servers = {
-		{ uri = 'localhost:3301', zone = '1' }, -- Shard1-master
-		{ uri = 'localhost:3302', zone = '2' }, -- Shard2-slave
-		{ uri = '192.168.1.152:3301', zone = '3' }, -- Shard2-master
-		{ uri = '192.168.1.152:3302', zone = '4' }, -- Shard1-slave
+		{ uri = '192.168.1.152:3301', zone = '1' }, -- Shard2-master
+                { uri = '192.168.1.152:3302', zone = '2' }, -- Shard1-slave
+		{ uri = 'localhost:3301', zone = '3' }, -- Shard1-master
+		{ uri = 'localhost:3302', zone = '4' }, -- Shard2-slave
 	},
 	login		=	'student26',
 	password	=	'fobloi56',
