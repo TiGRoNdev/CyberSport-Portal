a51fb839895d08404c77342efdc6da1e718ca65b
TiGRoNdev <tigron.dev@gmail.com>
2018-02-01 20:03:50 +0000

    Added replication param to db2 conf

--- db2/shard2-master.lua
+++ db2/shard2-master.lua
@@ -73,7 +73,7 @@ local cfg = {
         login           =       'student26',
         password        =       'fobloi56',
         redundancy      =       2,
-        binary          =       3301,
+	replication	=	true
 }
 
 shard = require('shard')
