b4e9ce06ca3e797dfb76c8faeb60dbf2329cee36
TiGRoNdev <tigron.dev@gmail.com>
2018-02-02 14:03:53 +0000

    Configured shard1-master

--- db1/init.sh
+++ db1/init.sh
@@ -1,18 +1,5 @@
-cd /home/student26
-mkdir tnt_shard1_master
-cd tnt_shard1_master
-mkdir xlogs
-mkdir snaps
-mkdir cold-data
-touch tnt_shard1_master.log
-
-cd /home/student26
-mkdir tnt_shard2_slave
-cd tnt_shard2_slave
-mkdir xlogs
-mkdir snaps
-mkdir cold-data
-touch tnt_shard2_slave.log
-
-cd /home/student26/CyperSport-portal/db1
-
+rm -r /var/lib/tarantool/*
+rm -r /var/log/tarantool/*
+rm /etc/tarantool/instances.available/shard*
+cp shard1-master.lua /etc/tarantool/instances.available/shard1-master.lua
+tarantoolctl start shard1-master
