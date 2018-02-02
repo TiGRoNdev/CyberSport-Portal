a0f5e8b8655ce16652cbb5c7d1fd1a000e04ac83
TiGRoNdev <tigron.dev@gmail.com>
2018-02-02 15:25:30 +0000

    Added deletions for init script

--- db2/init.sh
+++ db2/init.sh
@@ -1,4 +1,5 @@
 rm -r /var/lib/tarantool/*
 rm -r /var/log/tarantool/*
+rm /etc/tarantool/instances.available/shard*
 cp shard1-slave.lua /etc/tarantool/instances.available/shard1-slave.lua
 tarantoolctl start shard1-slave
