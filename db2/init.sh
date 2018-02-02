8579fe6b607273c8e92cc4d7bb45509ced6a16a1
TiGRoNdev <tigron.dev@gmail.com>
2018-02-02 15:19:53 +0000

    Changed init configs

--- db2/init.sh
+++ db2/init.sh
@@ -1,18 +1,4 @@
-cd /home/student26
-mkdir tnt_shard2_master
-cd tnt_shard2_master
-mkdir xlogs
-mkdir snaps
-mkdir cold-data
-touch tnt_shard2_master.log
-
-cd /home/student26
-mkdir tnt_shard1_slave
-cd tnt_shard1_slave
-mkdir xlogs
-mkdir snaps
-mkdir cold-data
-touch tnt_shard1_slave.log
-
-cd /home/student26/CyperSport-portal/db2
-
+rm -r /var/lib/tarantool/*
+rm -r /var/log/tarantool/*
+cp shard1-slave.lua /etc/tarantool/instances.available/shard1-slave.lua
+tarantoolctl start shard1-slave
--- db2/shard1-slave.lua
+++ db2/shard1-slave.lua
@@ -1,42 +1,208 @@
-#! /usr/bin/tarantool
+-- This is default tarantool initialization file
+-- with easy to use configuration examples including
+-- replication, sharding and all major features
+-- Complete documentation available in:  http://tarantool.org/doc/
+--
+-- To start this instance please run `systemctl start tarantool@example` or
+-- use init scripts provided by binary packages.
+-- To connect to the instance, use "sudo tarantoolctl enter example"
+-- Features:
+-- 1. Database configuration
+-- 2. Binary logging and automatic checkpoints
+-- 3. Replication
+-- 4. Automatinc sharding
+-- 5. Message queue
+-- 6. Data expiration
 
+-----------------
+-- Configuration
+-----------------
+box.cfg {
+    ------------------------
+    -- Network configuration
+    ------------------------
 
-box.cfg{
-	listen		=	3302,
-	replication	=	'student26:fobloi56@192.168.1.45:3301',
-	read_only	=	true,
-	log		=	'file:tnt_shard1_slave.log',
-	log_format	=	'json',
-	work_dir	=	'/home/student26/tnt_shard1_slave',
-	wal_dir		=	'xlogs',
-	memtx_dir	=	'snaps',
-	vinyl_dir	=	'cold-data',
-	username	=	'student26',
-	memtx_memory	=	2147483648,
-	checkpoint_interval	=	1800,
-	replication     =       {'replicator:pass@192.168.1.45:3301',
-                                 'replicator:pass@localhost:3302'}
+    -- The read/write data port number or URI
+    -- Has no default value, so must be specified if
+    -- connections will occur from remote clients
+    -- that do not use “admin address”
+    listen = '192.168.1.152:3302';
+    -- listen = '*:3301';
+
+    -- The server is considered to be a Tarantool replica
+    -- it will try to connect to the master
+    -- which replication_source specifies with a URI
+    -- for example konstantin:secret_password@tarantool.org:3301
+    -- by default username is "guest"
+
+    replication = 'tnt:tnt@192.168.1.45:3301';  -- master URI
+    read_only = true;
+
+    -- The server will sleep for io_collect_interval seconds
+    -- between iterations of the event loop
+    io_collect_interval = nil;
+
+    -- The size of the read-ahead buffer associated with a client connection
+    readahead = 16320;
+
+    ----------------------
+    -- Memtx configuration
+    ----------------------
+
+    -- An absolute path to directory where snapshot (.snap) files are stored.
+    -- If not specified, defaults to /var/lib/tarantool/INSTANCE
+    -- memtx_dir = nil;
+
+    -- How much memory Memtx engine allocates
+    -- to actually store tuples, in bytes.
+    memtx_memory = 1500 * 1024 * 1024; -- 128Mb
+
+    -- Size of the smallest allocation unit, in bytes.
+    -- It can be tuned up if most of the tuples are not so small
+    memtx_min_tuple_size = 16;
+
+    -- Size of the largest allocation unit, in bytes.
+    -- It can be tuned up if it is necessary to store large tuples
+    memtx_max_tuple_size = 128 * 1024 * 1024; -- 128Mb
+
+    -- Reduce the throttling effect of box.snapshot() on
+    -- INSERT/UPDATE/DELETE performance by setting a limit
+    -- on how many megabytes per second it can write to disk
+    -- memtx_snap_io_rate_limit = nil;
+
+    ----------------------
+    -- Vinyl configuration
+    ----------------------
+
+    -- An absolute path to directory where Vinyl files are stored.
+    -- If not specified, defaults to /var/lib/tarantool/INSTANCE
+    -- vinyl_dir = nil;
+
+    -- How much memory Vinyl engine can use for in-memory level, in bytes.
+    vinyl_memory = 2048 * 1024 * 1024; -- 128Mb
+
+    -- How much memory Vinyl engine can use for caches, in bytes.
+    vinyl_cache = 128 * 1024 * 1024; -- 128Mb
+
+    -- Size of the largest allocation unit, in bytes.
+    -- It can be tuned up if it is necessary to store large tuples
+    vinyl_max_tuple_size = 128 * 1024 * 1024; -- 128Mb
+
+    -- The maximum number of background workers for compaction.
+    vinyl_write_threads = 2;
+
+    ------------------------------
+    -- Binary logging and recovery
+    ------------------------------
+
+    -- An absolute path to directory where write-ahead log (.xlog) files are
+    -- stored. If not specified, defaults to /var/lib/tarantool/INSTANCE
+    -- wal_dir = nil;
+
+    -- Specify fiber-WAL-disk synchronization mode as:
+    -- "none": write-ahead log is not maintained;
+    -- "write": fibers wait for their data to be written to the write-ahead log;
+    -- "fsync": fibers wait for their data, fsync follows each write;
+    wal_mode = "none";
+
+    -- The maximal size of a single write-ahead log file
+    wal_max_size = 256 * 1024 * 1024;
+
+    -- The interval between actions by the checkpoint daemon, in seconds
+    checkpoint_interval = 60 * 60; -- one hour
+
+    -- The maximum number of checkpoints that the daemon maintans
+    checkpoint_count = 6;
+
+    -- Don't abort recovery if there is an error while reading
+    -- files from the disk at server start.
+    force_recovery = true;
+
+    ----------
+    -- Logging
+    ----------
+
+    -- How verbose the logging is. There are six log verbosity classes:
+    -- 1 – SYSERROR
+    -- 2 – ERROR
+    -- 3 – CRITICAL
+    -- 4 – WARNING
+    -- 5 – INFO
+    -- 6 – VERBOSE
+    -- 7 – DEBUG
+    log_level = 5;
+
+    -- By default, the log is sent to /var/log/tarantool/INSTANCE.log
+    -- If logger is specified, the log is sent to the file named in the string
+    -- logger = "example.log";
+
+    -- If true, tarantool does not block on the log file descriptor
+    -- when it’s not ready for write, and drops the message instead
+    log_nonblock = true;
+
+    -- If processing a request takes longer than
+    -- the given value (in seconds), warn about it in the log
+    too_long_threshold = 0.5;
+
+    -- Inject the given string into server process title
+    -- custom_proc_title = 'example';
 }
 
-box.schema.user.create('replicator', {password = 'pass', if_not_exists = true})
-box.schema.user.grant('replicator', 'replication', {if_not_exists = true}) -- grant replication role
+local function bootstrap()
+    -- Comment this if you need fine grained access control (without it, guest
+    -- will have access to everything)
+    box.schema.user.create('tnt', { password = 'tnt' })
+    box.schema.user.grant('tnt', 'read,write,execute', 'universe')
 
+    -- Keep things safe by default
+    --  box.schema.user.create('example', { password = 'secret' })
+    --  box.schema.user.grant('example', 'replication')
+    --  box.schema.user.grant('example', 'read,write,execute', 'space', 'example')
+end
 
--- Create USER for db
-box.schema.user.create('student26', {password = 'fobloi56', if_not_exists = true})
+-- for first run create a space and add set up grants
+box.once('SHARD-1-SLAVE', bootstrap)
 
-local cfg = {
-	servers = {
-		{ uri = '192.168.1.45:3301', zone = '1' }, -- Shard1-master
-		{ uri = '192.168.1.45:3302', zone = '2' }, -- Shard2-slave
-		{ uri = 'localhost:3301', zone = '2' }, -- Shard2-master
-		{ uri = 'localhost:3302', zone = '1' }, -- Shard1-slave
-	},
-	login		=	'student26',
-	password	=	'fobloi56',
-	redundancy	=	2,
-	replication	=	true
-}
+-----------------------
+-- Automatinc sharding
+-----------------------
+-- N.B. you need install tarantool-shard package to use shadring
+-- Docs: https://github.com/tarantool/shard/blob/master/README.md
+-- Example:
+--  local shard = require('shard')
+--  local shards = {
+--      servers = {
+--          { uri = [[host1.com:4301]]; zone = [[0]]; };
+--          { uri = [[host2.com:4302]]; zone = [[1]]; };
+--      };
+--      login = 'tester';
+--      password = 'pass';
+--      redundancy = 2;
+--      binary = '127.0.0.1:3301';
+--      monitor = false;
+--  }
+--  shard.init(shards)
 
-shard = require('shard')
-shard.init(cfg)
+-----------------
+-- Message queue
+-----------------
+-- N.B. you need to install tarantool-queue package to use queue
+-- Docs: https://github.com/tarantool/queue/blob/master/README.md
+-- Example:
+--  local queue = require('queue')
+--  queue.create_tube(tube_name, 'fifottl')
+
+-------------------
+-- Data expiration
+-------------------
+-- N.B. you need to install tarantool-expirationd package to use expirationd
+-- Docs: https://github.com/tarantool/expirationd/blob/master/README.md
+-- Example (deletion of all tuples):
+--  local expirationd = require('expirationd')
+--  local function is_expired(args, tuple)
+--    return true
+--  end
+--  expirationd.start("clean_all", space.id, is_expired {
+--    tuple_per_item = 50,
+--    full_scan_time = 3600
+--  })
